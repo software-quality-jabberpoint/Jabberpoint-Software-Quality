c7339bc670ef2969ad53a5b5f3a9cb64ec782247
beyza ölmez <beyza.olmez@student.nhlstenden.com>
2026-03-23 14:16:46 +0100

    Add unit tests for presentation model, slide, and style

---
+++ test/AllTests.java
@@ -0,0 +1,8 @@
+public class AllTests {
+	public static void main(String[] args) {
+		PresentationTest.runAll();
+		SlideTest.runAll();
+		StyleTest.runAll();
+		System.out.println("All tests passed.");
+	}
+}
